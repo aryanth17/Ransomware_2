abce37cb3a63973b8919decbb44b1b908f422153
chaitanya167 <61372406+chaitanya167@users.noreply.github.com>
2024-10-30 01:08:47 -0500

    Initial commit

---
+++ encrypt.py
@@ -0,0 +1,33 @@
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes, padding
+import os
+
+def derive_key(password, salt):
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    return kdf.derive(password.encode())
+
+def encrypt_file(file_path, password):
+    salt = os.urandom(16)
+    iv = os.urandom(16)
+    key = derive_key(password, salt)
+
+    with open(file_path, 'rb') as f:
+        data = f.read()
+
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(data) + padder.finalize()
+
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    encrypted_data = encryptor.update(padded_data) + encryptor.finalize()
+
+    with open(file_path, 'wb') as f:
+        f.write(salt + iv + encrypted_data)
